16bfb3d35b00dfb19b097580ae516819686cdbc7
Pawel <pawel@tomandco.co.uk>
2015-05-09 04:02:38 +0200

    initial

---
+++ TDWarlockDps.lua
@@ -0,0 +1,158 @@
+﻿-- Author      : Kaminari
+-- Create Date : 10/27/2014 6:47:46 PM
+
+-- SPELLS
+local _Corruption			= 172;
+local _ShadowBolt			= 686;
+local _Metamorphosis		= 103958;
+local _HandOfGuldan			= 105174;
+local _SoulFire				= 104027;
+local _Doom					= 603;
+local _TouchOfChaos			= 103964;
+local _ChaosWave			= 124916
+local _DarkSoulKnowledge	= 113861;
+local _GrimoireDoomguard	= 157900;
+local _Cataclysm			= 152108;
+local _DarkSoul				= 113861;
+
+-- AURAS
+local _MoltenCore = 140074;
+
+local isCataclysm = false;
+local isGrimoireOfService = false;
+local isDemonbolt = false;
+
+----------------------------------------------
+-- Pre enable, checking talents
+----------------------------------------------
+TDWarlockDps_CheckTalents = function()
+	isCataclysm = TDTalentEnabled('Cataclysm');
+	isDemonbolt = TDTalentEnabled('Demonbolt');
+	isGrimoireOfService = TDTalentEnabled('Grimoire of Service');
+end
+
+----------------------------------------------
+-- Enabling Addon
+----------------------------------------------
+function TDWarlockDps_EnableAddon(mode)
+	mode = mode or 1;
+	_TD['DPS_Description'] = 'TD Warlock DPS supports: Elemental';
+	_TD['DPS_OnEnable'] = TDWarlockDps_CheckTalents;
+	if mode == 1 then
+		_TD['DPS_NextSpell'] = TDWarlockDps_Demonology
+	end;
+	TDDps_EnableAddon();
+end
+
+----------------------------------------------
+-- Main rotation: Demonology
+----------------------------------------------
+TDWarlockDps_Demonology = function()
+
+	local timeShift, casting = TDEndCast();
+
+	local meta = TDWarlockDps_Metamorphosis();
+	local fury = UnitPower('player', 15);
+	local corruption = TDTargetAura(_Corruption, timeShift + 4);
+	local dsCD, dsCharges, dsMax = TDDps_SpellCharges(_DarkSoul);
+	local moltenCore = TDWarlockDps_MoltenCore();
+	local targetPh = TD_TargetPercentHealth();
+	local doom = TDTargetAura('Doom', timeShift + 18);
+	local hogCD, hogCharges, hogMax = TDDps_SpellCharges(_HandOfGuldan);
+
+	if casting == 'Soul Fire' and moltenCore > 0 then
+		moltenCore = moltenCore - 1;
+	end
+
+	if meta then
+	
+		if fury <= 40 then
+			return _Metamorphosis;
+		end
+		
+		if fury < 100 and fury >= 60 then
+			return _Corruption; -- same slot as Doom
+		end
+	
+		if not TDTargetAura(_Corruption, timeShift + 8) then
+			return _ShadowBolt; -- same slot as Touch of Chaos 
+		end
+		
+		if isCataclysm and TDDps_SpellCooldown(_Cataclysm, timeShift) then
+			return _Cataclysm;
+		end
+		
+		if not doom then
+			return _Corruption; -- same slot as Doom
+		end
+		
+		if hogCharges >= hogMax then
+			return _HandOfGuldan;
+		end
+		
+		if hogCharges >= (hogMax - 1) and hogCD < 6 + timeShift then
+			return _HandOfGuldan;
+		end
+		
+		if moltenCore > 0 then
+			return _SoulFire;
+		end
+	else
+		-- Not in metamorphosis
+		local hogDot = TDTargetAura(_HandOfGuldan, 3);
+		
+		if not corruption then
+			return _Corruption;
+		end
+		
+		if hogCharges >= hogMax then
+			return _HandOfGuldan;
+		end
+		
+		if hogCharges >= (hogMax - 1) and hogCD < 6 + timeShift then
+			return _HandOfGuldan;
+		end
+		
+		if fury >= 200 and not doom then
+			return _Metamorphosis;
+		end
+		
+		if moltenCore > 8 then
+			return _SoulFire;
+		end
+		
+		if moltenCore > 0 and targetPh < 0.25 then
+			return _SoulFire;
+		end
+		
+		if fury > 850 then
+			return _Metamorphosis;
+		end
+		
+	end
+	
+	return _ShadowBolt;
+	
+end
+
+----------------------------------------------
+-- Molten Core stacks
+----------------------------------------------
+function TDWarlockDps_MoltenCore()
+	local _, _, _, count, _, _, expirationTime = UnitAura('player', 'Molten Core'); 
+	if expirationTime ~= nil and (expirationTime - GetTime()) > 0.2 then
+		return count;
+	end
+	return 0;
+end
+
+----------------------------------------------
+-- Is in Metamorphosis
+----------------------------------------------
+function TDWarlockDps_Metamorphosis()
+	local is = UnitAura('player', 'Metamorphosis');
+	return is == 'Metamorphosis';
+end
+
+
+
