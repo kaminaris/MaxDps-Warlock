41b82922883270b4e0cd6d84f13efc31e89b6bfa
Pawel <zacdbg@gmail.com>
2016-10-08 05:33:43 +0200

    v.1.4 - legion update, destro demo

--- main.lua
+++ main.lua
@@ -1,6 +1,18 @@
 ﻿-- Author      : Kaminari
 -- Create Date : 10/27/2014 6:47:46 PM
 
+-- NEW SPELLS
+local _DimensionalRift = 196586;
+local _Eradication = 196412;
+local _LifeTap = 1454;
+local _SummonDarkglare = 205180;
+local _CallDreadstalkers = 104316;
+local _GrimoireFelguard = 111898;
+local _DemonicEmpowerment = 193396;
+local _SoulHarvest = 196098;
+local _ThalkielsConsumption = 211714;
+local _Demonbolt = 157695;
+
 -- SPELLS
 local _Corruption			= 172;
 local _ShadowBolt			= 686;
@@ -40,7 +52,11 @@ local isGrimoireOfService = false;
 local isDemonbolt = false;
 local isSoulburnHaunt = false;
 local isCharredRemains = false;
-local isDemonicServitude = false;
+local isSupremacy = false;
+local isShadowburn = false;
+local isEradication = false;
+local isSummonDarkglare = false;
+local isSoulHarvest = false;
 
 ----------------------------------------------
 -- Pre enable, checking talents
@@ -51,7 +67,11 @@ TDDps_Warlock_CheckTalents = function()
 	isGrimoireOfService = TD_TalentEnabled('Grimoire of Service');
 	isSoulburnHaunt = TD_TalentEnabled('Soulburn: Haunt');
 	isCharredRemains = TD_TalentEnabled('Charred Remains');
-	isDemonicServitude = TD_TalentEnabled('Demonic Servitude');
+	isSummonDarkglare = TD_TalentEnabled('Summon Darkglare');
+	isSupremacy = TD_TalentEnabled('Grimoire of Supremacy');
+	isShadowburn = TD_TalentEnabled('Shadowburn');
+	isEradication = TD_TalentEnabled('Eradication');
+	isSoulHarvest = TD_TalentEnabled('Soul Harvest');
 end
 
 ----------------------------------------------
@@ -79,67 +99,6 @@ end
 TDDps_Warlock_Affliction = function()
 	local timeShift, currentSpell, gcd = TD_EndCast();
 
-	local shards = UnitPower('player', SPELL_POWER_SOUL_SHARDS);
-	local dsCD, dsCharges, dsMax = TD_SpellCharges(_DarkSoulMisery, timeShift);
-
-	local corruption = TD_TargetAura(_Corruption, timeShift + 5);
-	local agony = TD_TargetAura(_Agony, timeShift + 7);
-	local ua = TD_TargetAura(_UnstableAffliction, timeShift + 4);
-
-	local dsm = TD_Aura(_DarkSoulMisery, timeShift);
-	local haunting = TD_Aura(_HauntingSpirits, timeShift + 9);
-	local sb = TD_Aura(_Soulburn, timeShift);
-	local bl = TD_Bloodlust(timeShift);
-
-	local gd = TD_SpellAvailable(_GrimoireDoomguard, timeShift);
-	local doomguard = TD_SpellAvailable(_SummonDoomguard, timeShift);
-	local felstorm = TD_SpellAvailable(_Felstorm);
-
-	local targetPh = TD_TargetPercentHealth();
-
-	local cata = TD_SpellAvailable(_Cataclysm, timeShift);
-
-	TDButton_GlowCooldown(_GrimoireDoomguard, gd);
-
-	if isDemonicServitude then
-		TDButton_GlowCooldown(_SummonDoomguard, doomguard);
-	end
-
-	TDButton_GlowCooldown(_DarkSoulMisery, dsCharges > 0 and not dsm);
-
-	if isCataclysm then
-		TDButton_GlowCooldown(_Cataclysm, cata);
-	end
-
-	-- keep and apply all 3 dots
-	if not agony then
-		return _Agony;
-	end
-
-	if not corruption then
-		return _Corruption;
-	end
-
-	if not ua then
-		return _UnstableAffliction;
-	end
-
-	if isSoulburnHaunt and not haunting and not sb and shards > 1 then
-		return _Soulburn;
-	end
-
-	if isSoulburnHaunt and sb and shards > 0 then
-		return _Haunt;
-	end
-
-	if shards >= 4 then
-		return _Haunt;
-	end
-
-	if (bl or dsm) and ((isSoulburnHaunt and shards >= 3) or (not isSoulburnHaunt and shards > 0)) then
-		return _Haunt;
-	end;
-
 	return _DrainSoul;
 end
 
@@ -149,94 +108,78 @@ end
 TDDps_Warlock_Demonology = function()
 	local timeShift, currentSpell, gcd = TD_EndCast();
 
-	local meta = TDDps_Warlock_Metamorphosis();
-	local fury = UnitPower('player', 15);
-	local corruption = TD_TargetAura(_Corruption, timeShift + 4);
-	local dsk = TD_TargetAura(_DarkSoulKnowledge, timeShift);
-	local dsCD, dsCharges, dsMax = TD_SpellCharges(_DarkSoulKnowledge);
-	local gd = TD_SpellAvailable(_GrimoireDoomguard, timeShift);
-	local felstorm = TD_SpellAvailable(_Felstorm);
-	local moltenCore = TDDps_Warlock_MoltenCore();
-	local targetPh = TD_TargetPercentHealth();
-	local doom = TD_TargetAura('Doom', timeShift + 18);
-	local hogCD, hogCharges, hogMax = TD_SpellCharges(_HandOfGuldan);
-	local cata = TD_SpellAvailable(_Cataclysm, timeShift);
-	if currentSpell == 'Soul Fire' and moltenCore > 0 then
-		moltenCore = moltenCore - 1;
+	local ss = UnitPower('player', SPELL_POWER_SOUL_SHARDS);
+	local mana = TD_Mana(0, timeShift)
+
+	local doom = TD_TargetAura(_Doom, timeShift + 5);
+	local demoEmp = TD_UnitAura(_DemonicEmpowerment, timeShift + 5, 'pet');
+
+	local sdk = TD_SpellAvailable(_SummonDarkglare, timeShift);
+	local callD = TD_SpellAvailable(_CallDreadstalkers, timeShift);
+	local felg = TD_SpellAvailable(_GrimoireFelguard, timeShift);
+	local harv = TD_SpellAvailable(_SoulHarvest, timeShift);
+	local doomguard = TD_SpellAvailable(_SummonDoomguard, timeShift);
+	local tk = TD_SpellAvailable(_ThalkielsConsumption, timeShift);
+	local felstorm = TD_SpellAvailable(_Felstorm, timeShift);
+
+	if currentSpell == 'Call Dreadstalkers' then
+		ss = ss - 2;
+	elseif currentSpell == 'Hand of Gul\'dan' then
+		ss = ss - 4;
+		if ss < 0 then
+			ss = 0;
+		end
 	end
 
-	if isCataclysm then
-		TDButton_GlowCooldown(_Cataclysm, cata);
+	if not isSupremacy then
+		TDButton_GlowCooldown(_SummonDoomguard, doomguard);
 	end
 
-	TDButton_GlowCooldown(_GrimoireDoomguard, gd);
-	TDButton_GlowCooldown(_Felstorm, felstorm);
-	TDButton_GlowCooldown(_DarkSoulKnowledge, dsCharges > 0 and not dsk);
+	if mana < 0.2 then
+		return _LifeTap;
+	end
 
-	if meta then
-	
-		if fury <= 40 then
-			return _Metamorphosis;
-		end
-		
-		if fury < 100 and fury >= 60 then
-			return _Corruption; -- same slot as Doom
-		end
-	
-		if not TD_TargetAura(_Corruption, timeShift + 8) then
-			return _ShadowBolt; -- same slot as Touch of Chaos 
-		end
-		
-		if not doom then
-			return _Corruption; -- same slot as Doom
-		end
-		
-		if hogCharges >= hogMax then
-			return _HandOfGuldan;
-		end
-		
-		if hogCharges >= (hogMax - 1) and hogCD < 6 + timeShift then
-			return _HandOfGuldan;
-		end
-		
-		if moltenCore > 0 then
-			return _SoulFire;
-		end
+	if not doom then
+		return _Doom;
+	end
+
+	if isSummonDarkglare and sdk and ss > 0 then
+		return _SummonDarkglare;
+	end
+
+	if callD and ss > 1 and currentSpell ~= 'Call Dreadstalkers' then
+		return _CallDreadstalkers;
+	end
+
+	if isGrimoireOfService and felg and ss > 0 then
+		return _GrimoireFelguard;
+	end
+
+	if ss > 3 and currentSpell ~= 'Hand of Gul\'dan' then
+		return _HandOfGuldan;
+	end
+
+	if not demoEmp and currentSpell ~= 'Demonic Empowerment' then
+		return _DemonicEmpowerment;
+	end
+
+	if isSoulHarvest and harv then
+		return _SoulHarvest;
+	end
+
+	if tk and currentSpell ~= 'Thal\'kiel\'s Consumption' then
+		return _ThalkielsConsumption;
+	end
+
+	if felstorm then
+		return _Felstorm;
+	end
+
+	if isDemonbolt then
+		return _Demonbolt;
 	else
-		-- Not in metamorphosis
-		local hogDot = TD_TargetAura(_HandOfGuldan, 3);
-		
-		if not corruption then
-			return _Corruption;
-		end
-		
-		if hogCharges >= hogMax then
-			return _HandOfGuldan;
-		end
-		
-		if hogCharges >= (hogMax - 1) and hogCD < 6 + timeShift then
-			return _HandOfGuldan;
-		end
-		
-		if fury >= 200 and not doom then
-			return _Metamorphosis;
-		end
-		
-		if moltenCore > 8 then
-			return _SoulFire;
-		end
-		
-		if moltenCore > 0 and targetPh < 0.25 then
-			return _SoulFire;
-		end
-		
-		if fury > 850 then
-			return _Metamorphosis;
-		end
-		
+		return _ShadowBolt;
 	end
-	
-	return _ShadowBolt;
 end
 
 ----------------------------------------------
@@ -245,64 +188,61 @@ end
 TDDps_Warlock_Destruction = function()
 	local timeShift, currentSpell, gcd = TD_EndCast();
 
-	local embers = UnitPower('player', SPELL_POWER_BURNING_EMBERS, true);
+	local ss = UnitPower('player', SPELL_POWER_SOUL_SHARDS);
 
-	local dsCD, dsCharges, dsMax = TD_SpellCharges(_DarkSoulInstability, timeShift);
+	local drCD, drCharges, drMax = TD_SpellCharges(_DimensionalRift, timeShift);
 	local conCD, conCharges, conMax = TD_SpellCharges(_Conflagrate, timeShift);
 
 	local immo = TD_TargetAura(_Immolate, timeShift + 5);
+	local health = UnitHealth('target');
 
-	local dsi = TD_Aura(_DarkSoulInstability, timeShift);
-	local fab = TD_Aura(_FireAndBrimstone, timeShift);
+	local era = TD_TargetAura(_Eradication, timeShift);
+	local mana = TD_Mana(0, timeShift);
 
 	local gd = TD_SpellAvailable(_GrimoireDoomguard, timeShift);
 	local doomguard = TD_SpellAvailable(_SummonDoomguard, timeShift);
 	local havoc = TD_SpellAvailable(_Havoc, timeShift);
-	local felstorm = TD_SpellAvailable(_Felstorm);
-
-	local cbCost = 10;
-	if currentSpell == 'Chaos Bolt' then
-		if fab then
-			cbCost = 20;
-		end
-		embers = embers - cbCost;
-	end
 
 	local targetPh = TD_TargetPercentHealth();
 	local cata = TD_SpellAvailable(_Cataclysm, timeShift);
 
-	if not isDemonicServitude then
+	if not isSupremacy then
 		TDButton_GlowCooldown(_SummonDoomguard, doomguard);
 	end
 
+	if currentSpell == 'Chaos Bolt' then
+		ss = ss - 2;
+	end
+
 	TDButton_GlowCooldown(_GrimoireDoomguard, gd);
-	TDButton_GlowCooldown(_DarkSoulInstability, dsCharges > 0 and not dsi);
 	TDButton_GlowCooldown(_Havoc, havoc);
 
-	local chaosFlag = (embers >= 35 or (isCharredRemains and embers > 25)) or (dsi and embers >= 10);
-
-	if targetPh < 0.2 and chaosFlag then
-		return _Shadowburn;
+	if mana < 0.1 then
+		return _LifeTap;
 	end
 
 	if not immo and currentSpell ~= 'Immolate' then
 		return _Immolate;
 	end
 
-	if conCharges >=2 then
-		return _Conflagrate;
-	end
-
-	if isCataclysm and cata then
-		return _Cataclysm;
-	end
-
-	if chaosFlag then
+	if ss >= 5 then
 		return _ChaosBolt;
 	end
 
-	if conCharges >=1 then
+	if drCharges > 2 then
+		return _DimensionalRift
+	end
+
+	if isShadowburn and health < 500000 and ss > 0 then
+		return _Shadowburn;
+	end
+
+	if conCharges > 1 or (conCharges > 0 and conCD < 2) then
 		return _Conflagrate;
+	end
+
+	if isEradication and not era and ss > 1 and currentSpell ~= 'Chaos Bolt' then
+		return _ChaosBolt;
 	end
 
 	return _Incinerate;
